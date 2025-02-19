e1faff75ded809a767f9f22de105a77e759c2f77
jainaryan644 <62453609+jainaryan644@users.noreply.github.com>
2025-02-19 12:42:47 -0700

    Implemented TF-IDF vectorization for content based filtering.

--- recommender.py
+++ recommender.py
@@ -1,4 +1,25 @@
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-df = pd.read_csv("movies.csv")
-print(df.columns)
+
+def load_data(filepath):
+    #load dataset
+    df = pd.read_csv(filepath)
+
+    #print columns for dataset
+    print("Columns in dataset:", df.columns)
+    print(df.head())
+
+    return df
+
+def build_tfidf_matrix(df):
+    #text to tfidf matrix conversion
+    vectorizer = TfidfVectorizer(stop_words='english')
+    tfidf_matrix = vectorizer.fit_transform(df['Description'].fillna(''))
+    return vectorizer, tfidf_matrix
+
+
+if __name__ == "__main__":
+    path = "movies.csv"
+    df = load_data(path)
+    vectorizer, tfidf_matrix = build_tfidf_matrix(df)
