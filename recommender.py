5b93a6460fe9e5f84a7f657c46e942c77282dbfa
jainaryan644 <62453609+jainaryan644@users.noreply.github.com>
2025-02-19 14:46:22 -0700

    Improved recommendations by adding combined text and bigram TF-IDF. Inlcuded warning message for low similarity to account for edge case.

--- recommender.py
+++ recommender.py
@@ -6,17 +6,23 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 def load_data(filepath):
     #load dataset
-    df = pd.read_csv(filepath)
+    df = pd.read_csv(filepath, encoding="ISO-8859-1")
 
     #print columns for dataset
-    print("Columns in dataset:", df.columns)
-    print(df.head())
+    #print("Columns in dataset:", df.columns)
+    #print(df.head())
+
+    df['combined_text'] = (
+        df['Description'].fillna('') + ' ' +
+        df['Stars'].fillna('') + ' ' +
+        df['Director'].fillna('') 
+    )
 
     return df
 
 #text to tfidf matrix conversion
 def build_tfidf_matrix(df):
-    vectorizer = TfidfVectorizer(stop_words='english')
+    vectorizer = TfidfVectorizer(stop_words='english', ngram_range=(1,2)) #bigrams for better context
     tfidf_matrix = vectorizer.fit_transform(df['Description'].fillna(''))
     return vectorizer, tfidf_matrix
 
@@ -25,6 +31,10 @@ def recommend_movies(user_query, df, vectorizer, tfidf_matrix, top_n=5):
     query_vec = vectorizer.transform([user_query])
     similarity_scores = cosine_similarity(query_vec, tfidf_matrix).flatten()
     top_indices = np.argsort(similarity_scores)[::-1][:top_n]
+
+    if similarity_scores[top_indices[0]] < 0.05:
+        print("Your query may not have strong matches")
+
     recommended_df = df.iloc[top_indices][['Name']]
 
     #retrieve descriptions separately
